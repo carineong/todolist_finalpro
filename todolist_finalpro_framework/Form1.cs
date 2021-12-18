f1478d97b59cbf9c22170548bc68a5e18a99526b
Sherie Low <sherie.low.wl@gmail.com>
2021-12-19 04:41:44 +0800

    改了一些颜色 (没实现什么代码)

--- todolist_finalpro_framework/Form1.cs
+++ todolist_finalpro_framework/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
 
 namespace todolist_finalpro_framework
 {
-    public partial class Form1 : Form
+    public partial class Form1 : KryptonForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void monthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            // calendar里的selected date改了以后在这里写代码
         }
     }
 }
